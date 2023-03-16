e7b222fd12a36ca9730816fe36e8e543b45b3768
Andre <meirezende@hotmail.com>
2023-03-16 11:49:35 -0300

    New JPA Repository classes

---
+++ budget-repository-jpa/src/main/java/br/com/rezende/budget/db/ServiceEntity.java
@@ -0,0 +1,24 @@
+package br.com.rezende.budget.db;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(schema="budget_context", name = "service",
+        indexes = {
+                @Index(name = "name", columnList = "name"),
+                @Index(name = "unity", columnList = "unity")
+        })
+public class ServiceEntity {
+    @Id
+    private String id;
+
+    private String name;
+
+    private String unity;
+    private boolean active;
+
+    @Version
+    private Long version;
+}
